8adf03c3cb4f3e95c9f74375d36886fdd1654d20
Faranak <sharifi_faranak@live.ca>
2019-07-22 15:31:05 -0400

    uploaded website template

---
+++ CISC-282-A4-Sample-Solution/js/slider.js
@@ -0,0 +1,11 @@
+/******************************************************************************/
+/* Slick Slider */
+/******************************************************************************/
+$(document).ready(function() {
+	$('.photo-slider').slick({
+		dots: true,
+		infinite: true,
+		centerMode: true,
+		variableWidth: true
+	});
+});
